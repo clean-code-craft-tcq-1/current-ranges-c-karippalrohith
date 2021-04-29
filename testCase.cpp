a0c61c94392a28127657594664c68ef50c33f377
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-29 12:46:36 +0530

    Update testCase.cpp

--- testCase.cpp
+++ testCase.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
+#include "BMS_Current_Validation.h"
 
 TEST_CASE("Check if all the values are within the range") 
 {  
