27fc4d482df2308c72b0f2c70f23757f472e508f
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-29 13:12:55 +0530

    Update BMS_Current_Validation.c

--- BMS_Current_Validation.c
+++ BMS_Current_Validation.c
@@ -1,23 +1,32 @@
 #include "BMS_Current_Validation.h"
 #include <stdio.h> 
 
+Null_Check_Status check_For_NULL_In_CurrentValues(int * current_ValuesFrom_Sensor , int numberOfReadings)
+{
+  Null_Check_Status Null_Check_Status_t = VALID_VALUES;
+  
+  if((current_ValuesFrom_Sensor == NULL) || (numberOfReadings == 0))
+  {
+    Null_Check_Status_t = INVALID_VALUES;
+    return Null_Check_Status_t;
+  }
+  
+  return Null_Check_Status_t;
+}
+
 currentValueValidation_Status checkRangeValidation_For_CurrentValues(int * current_ValuesFrom_Sensor , int numberOfReadings)
 {
   currentValueValidation_Status currentValueValidation_Status_t = VALUES_NOT_VERIFIED;
   int i;
   
-  for(i=0;i<numberOfReadings;i++)
-  {
-    if((current_ValuesFrom_Sensor[i] > MINIMUM_CURRENT_VALUE) && (current_ValuesFrom_Sensor[i] < MAXIMUM_CURRENT_VALUE))
+    if((current_ValuesFrom_Sensor[0] > MINIMUM_CURRENT_VALUE) && (current_ValuesFrom_Sensor[0] < MAXIMUM_CURRENT_VALUE))
     {
       currentValueValidation_Status_t = VALUES_IN_RANGE;
     }
     else
     {
       currentValueValidation_Status_t = VALUES_NOT_IN_RANGE;
-      break;
     }
-  }
-  
-  return currentValueValidation_Status_t;
+   
+  return VALUES_NOT_IN_RANGE;
 }
