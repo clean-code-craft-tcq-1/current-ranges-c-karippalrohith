ceb6fb207db351c7496124541b1e2233848e25af
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-29 13:08:22 +0530

    Update BMS_Current_Validation.h

--- BMS_Current_Validation.h
+++ BMS_Current_Validation.h
@@ -7,4 +7,12 @@ typedef enum {
   VALUES_NOT_VERIFIED
 }currentValueValidation_Status;
 
+typedef enum {
+	VALID_VALUES,
+	INVALID_VALUES,
+	NO_CHECK_DONE
+}Null_Check_Status;
+
 currentValueValidation_Status checkRangeValidation_For_CurrentValues(int * current_ValuesFrom_Sensor , int numberOfReadings);
+
+Null_Check_Status check_For_NULL_In_CurrentValues(int * current_ValuesFrom_Sensor , int numberOfReadings);
