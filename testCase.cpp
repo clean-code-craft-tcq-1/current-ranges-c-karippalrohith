98010e4e5b33897cee1923916fc811f99a62a643
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-29 13:07:26 +0530

    Update testCase.cpp

--- testCase.cpp
+++ testCase.cpp
@@ -3,6 +3,14 @@
 #include "test/catch.hpp"
 #include "BMS_Current_Validation.h"
 
+TEST_CASE("NULL check for the samples") 
+{  
+  int current_ValuesFrom_Sensor[] = {3, 3, 5, 4, 10, 11, 12};
+  int numberOfReadings = sizeof(current_ValuesFrom_Sensor)/sizeof(current_ValuesFrom_Sensor[0]);
+  int current_Value_Status_NULL_Check = check_For_NULL_In_CurrentValues(NULL,numberOfReadings);
+  REQUIRE(current_Value_Status_NULL_Check == INVALID_VALUES);
+}
+
 TEST_CASE("Check if all the values are within the range") 
 {  
   int current_ValuesFrom_Sensor[] = {3, 3, 5, 4, 10, 11, 12};
