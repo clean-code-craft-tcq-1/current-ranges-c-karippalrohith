23636e1cc249196433edcf98c7f9088f9b523adc
karippalrohith <77481086+karippalrohith@users.noreply.github.com>
2021-04-29 12:34:01 +0530

    Update testCase.cpp

--- testCase.cpp
+++ testCase.cpp
@@ -2,8 +2,11 @@
 
 #include "test/catch.hpp"
 
-TEST_CASE("Test something") 
+TEST_CASE("Check if all the values are within the range") 
 {  
- // REQUIRE();
+  int current_ValuesFrom_Sensor[] = {3, 3, 5, 4, 10, 11, 12};
+  int numberOfReadings = sizeof(current_ValuesFrom_Sensor)/sizeof(current_ValuesFrom_Sensor[0]);
+  int current_Values_In_Range = checkRangeValidation_For_CurrentValues(current_ValuesFrom_Sensor,numberOfReadings);
+  REQUIRE(current_Values_In_Range == VALUES_IN_RANGE);
 }
 
